76212102792e5aa599c6530f49266c77aca39791
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-02-16 17:06:01 -0300

    add lists

---
+++ app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,119 @@
+import { MdTune } from "react-icons/md";
+import { MdPersonAddAlt1} from "react-icons/md";
+import { MdSwapVert } from "react-icons/md";
+import { MdRemoveRedEye } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
+
+import TableSearch from "../../(components)/TableSearch"
+import Pagination from "../../(components)/Pagination";
+import Table from "../../(components)/Table";
+import { role, parentsData } from "@/lib/data"; 
+import Link from "next/link";
+
+type Parent = {
+  id: number; 
+  name: string;
+  email?: string;
+  students: string[];
+  photo: string;
+  phone?: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student Name",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  }, 
+  {
+    header: "Email",
+    accessor: "email",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+const ListStudents = () => {
+  const renderRow = (item: Parent) => (
+    <tr
+      key={item.id}
+      className="border-b border-content/10 even:bg-stats-pastel_blue/20 text-sm hover:bg-stats-pastel_blue"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3> 
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.students.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.email}</td>  
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2 text-content/70">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-stats-mint_green">
+              <MdRemoveRedEye/>
+            </button>
+          </Link>
+         {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-stats-pastel_pink">
+               <MdDelete />
+               {/* <FormModal table="teacher" type="delete" id={item.id}/> */}
+            </button>
+          )} 
+        </div>
+      </td>
+    </tr>
+  );
+  return (
+    <div className="flex flex-col flex-1 gap-4 p-4  bg-white shadow-lg shadow-shadow rounded-xl m-4 ">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center justify-center text-center gap-2 text-lg text-content/70">
+            <button className="rounded-xl bg-stats-soft_yellow p-1">
+              <MdTune />
+            </button>
+            <button className="rounded-xl bg-stats-soft_yellow p-1">
+              <MdSwapVert />
+            </button>
+            {role === "admin" && ( 
+              <button className="rounded-xl bg-stats-soft_yellow p-1">
+                <MdPersonAddAlt1 />
+              </button>
+            )}
+          </div>
+
+        </div>
+      </div>
+
+      {/* List */}
+      <Table data={parentsData} columns={columns}  renderRow={renderRow}/>
+
+      {/* Pagination */}
+      <Pagination />
+    </div>
+  )
+}
+
+export default ListStudents
---
+++ app/(dashboard)/list/subjects/page.tsx
@@ -0,0 +1,100 @@
+import { MdTune } from "react-icons/md";
+import { MdPersonAddAlt1} from "react-icons/md";
+import { MdSwapVert } from "react-icons/md";
+import { MdRemoveRedEye } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+
+
+import TableSearch from "../../(components)/TableSearch"
+import Pagination from "../../(components)/Pagination";
+import Table from "../../(components)/Table";
+import { role, subjectsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type Subject = {
+  id: number; 
+  name: string;
+  teachers: string[];
+   
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Teachers",
+    accessor: "teacher",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+const ListStudents = () => {
+
+  const renderRow = (item: Subject) => (
+    <tr
+      key={item.id}
+      className="border-b border-content/10 even:bg-stats-pastel_blue/20 text-sm hover:bg-stats-pastel_blue"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3> 
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
+      <td>
+        <div className="flex items-center gap-2 text-content/70">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-stats-mint_green">
+              <MdRemoveRedEye/>
+            </button>
+          </Link>
+         {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-stats-pastel_pink">
+               <MdDelete />
+               {/* <FormModal table="teacher" type="delete" id={item.id}/> */}
+            </button>
+          )} 
+        </div>
+      </td>
+    </tr>
+  );
+  return (
+    <div className="flex flex-col flex-1 gap-4 p-4  bg-white shadow-lg shadow-shadow rounded-xl m-4 ">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All students</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center justify-center text-center gap-2 text-lg text-content/70">
+            <button className="rounded-xl bg-stats-soft_yellow p-1">
+              <MdTune />
+            </button>
+            <button className="rounded-xl bg-stats-soft_yellow p-1">
+              <MdSwapVert />
+            </button>
+            {role === "admin" && ( 
+              <button className="rounded-xl bg-stats-soft_yellow p-1">
+                <MdPersonAddAlt1 />
+              </button>
+            )}
+          </div>
+
+        </div>
+      </div>
+
+      {/* List */}
+      <Table data={subjectsData} columns={columns}  renderRow={renderRow}/>
+
+      {/* Pagination */}
+      <Pagination />
+    </div>
+  )
+}
+
+export default ListStudents
